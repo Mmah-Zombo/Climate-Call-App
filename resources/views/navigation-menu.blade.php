e80bb356bc51457242f7964822026c4e7956f0e8
M'mah Zombo <www.mariamzombo@gmail.com>
2023-10-13 12:15:26 +0000

    corrected the responsive nav menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -138,8 +138,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden mt-4 bg-white">  
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link href="{{ route('community') }}" :active="request()->routeIs('community')">
+                {{ __('Community') }}
             </x-responsive-nav-link>
         </div>
         <!-- Responsive Settings Options -->
