3be9b7cf6929d03a642f07c7df16bf633cf1c558
M'mah Zombo <www.mariamzombo@gmail.com>
2023-10-19 12:51:36 +0000

    removed unused files unnecessary comments and unsed functionalities

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,28 +33,15 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <a class="mr-4 underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
-                    {{ __('Register here!') }}
-                </a>
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-               
-            </div>
-            <div class="flex items-center justify-end mt-4">
-                <button class="ml-4 bg-amethyst text-white inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest hover:bg-teaGreen focus:bg-teaGreen hover:text-amethyst active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                    {{ __('Log in') }}
-                </button>
-            </div>
-            
-            {{-- <div class="w-ful h-fit text-right mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
                     {{ __('You don\'t have an account? Register here') }}
                 </a>
-            </div> --}}
+
+                <button type="submit" class="ml-4 bg-amethyst text-white inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest hover:bg-teaGreen focus:bg-teaGreen hover:text-amethyst active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    {{ __('Log in') }}
+                </button>
+            </div>
+
         </form>
     </x-authentication-card>
 </x-guest-layout>
